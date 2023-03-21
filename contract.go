d7e5b35e4d87e24225c929cef910f0a821067510
Travis Smith <travis@pop-os.localdomain>
2023-03-21 11:49:27 -0400

    Generates ID from national ID and country

--- contract.go
+++ contract.go
@@ -6,6 +6,7 @@ import (
 	"log"
 	"github.com/hyperledger/fabric-contract-api-go/contractapi"
 	"github.com/google/uuid"
+	"crypto/sha256"
 )
 
 
@@ -31,7 +32,8 @@ type Vaccination struct {
 }
 
 
-func (s *SmartContract) PatientExists(ctx contractapi.TransactionContextInterface, id string) (bool, error) {
+func (s *SmartContract) PatientExists(ctx contractapi.TransactionContextInterface, nationalID string, country string) (bool, error) {
+	id := createID(nationalID, country)
 	patientJSON, err := ctx.GetStub().GetState(id)
 	if err != nil {
 		return false, fmt.Errorf("Failed to read from world state: %v", err)
@@ -40,13 +42,14 @@ func (s *SmartContract) PatientExists(ctx contractapi.TransactionContextInterfac
 	return patientJSON != nil, nil
 }
 
-func (s *SmartContract) CreatePatient(ctx contractapi.TransactionContextInterface, id string, firstName string, lastName string, dateOfBirth string) error {
-	exists, err := s.PatientExists(ctx, id)
+func (s *SmartContract) CreatePatient(ctx contractapi.TransactionContextInterface, nationalID string, country string, firstName string, lastName string, dateOfBirth string) error {
+	id := createID(nationalID, country)
+	exists, err := s.PatientExists(ctx, nationalID, country)
 	if err != nil {
 		return err
 	}
 	if exists {
-		return fmt.Errorf("The patient %s already exists", id)
+		return fmt.Errorf("The patient with id: %s and country %s exists", nationalID, country)
 	}
 
 	patient := Patient{
@@ -64,13 +67,14 @@ func (s *SmartContract) CreatePatient(ctx contractapi.TransactionContextInterfac
 	return ctx.GetStub().PutState(id, patientJSON)
 }
 
-func (s *SmartContract) ReadPatient(ctx contractapi.TransactionContextInterface, id string) (*Patient, error) {
+func (s *SmartContract) ReadPatient(ctx contractapi.TransactionContextInterface, nationalID string, country string) (*Patient, error) {
+	id := createID(nationalID, country)
 	patientJSON, err := ctx.GetStub().GetState(id)
 	if err != nil {
 		return nil, fmt.Errorf("Failed to read from world state: %v", err)
 	}
 	if patientJSON == nil {
-		return nil, fmt.Errorf("The patient %s does not exist", id)
+		return nil, fmt.Errorf("The patient does not exist")
 	}
 
 	var patient Patient
@@ -82,8 +86,9 @@ func (s *SmartContract) ReadPatient(ctx contractapi.TransactionContextInterface,
 	return &patient, nil
 }
 
-func (s *SmartContract) AddVaccinationToPatient(ctx contractapi.TransactionContextInterface, patientID string, vaccineName string, vaccineDate string, healthCareProvider string) (*Patient, error) {
-	patient, err := s.ReadPatient(ctx, patientID)
+func (s *SmartContract) AddVaccinationToPatient(ctx contractapi.TransactionContextInterface, nationalID string, country string, vaccineName string, vaccineDate string, healthCareProvider string) (*Patient, error) {
+	patientID := createID(nationalID, country)
+	patient, err := s.ReadPatient(ctx, nationalID, country)
 	if err != nil {
 		return nil, err
 	}
@@ -109,6 +114,12 @@ func (s *SmartContract) AddVaccinationToPatient(ctx contractapi.TransactionConte
 	}
 
 	return patient, nil
+}
+
+// Create ID from nationalID and country
+func createID(nationalID string, country string) string {
+	hash := sha256.Sum256([]byte(nationalID + country))
+	return fmt.Sprintf("%x", hash)
 }
 
 
